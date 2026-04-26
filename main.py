aeb5c7ae230b25b85bd7d296563660dc6d570db3
tscott12331 <t.scott12331@gmail.com>
2026-04-25 18:49:25 -0700

    Add range parsing to find frame ranges under total video frames with hanldes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from typing import Tuple
 import pandas
 
 import pymongo
@@ -6,13 +7,34 @@ from pymongo.synchronous.collection import Collection
 from pymongo.synchronous.database import Database
 from pymongo.typings import _Pipeline
 from pathlib import Path
+import argparse
+import ffmpeg
 
-def import_csv(csv_path: Path, dump_name:str, db: Database) -> Collection:
-    data_frame = pandas.read_csv(csv_path)
-    data_frame = data_frame.fillna('')
-    data = data_frame.to_dict(orient='records')
-    col = db[dump_name]
-    col.insert_many(data)
+HANDLE_LEN = 2.0
+SECOND_FRAMES = 24
+MINUTE_FRAMES = SECOND_FRAMES * 60
+HOUR_FRAMES = MINUTE_FRAMES * 60
+
+
+def conf_argparse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-b', '--baselight', help="path to baselight file", required=True)
+    parser.add_argument('-x', '--xytech', help="path to xytech file", required=True)
+    
+    parser.add_argument('-p', '--process', help="path to video to process", required=True)
+
+    return parser.parse_args()
+
+def insert_frame_data(df: pandas.DataFrame, db: Database, col_name: str) -> Collection:
+    col = db[col_name]
+    
+    # # to persist data in developmennt
+    if col.find_one():
+        return col
+
+    col.insert_many(df.to_dict(orient='records'))
+
     return col
 
 # match path after prefix for xytech file paths
@@ -91,10 +113,105 @@ def get_frame_data(frame_file_path:str, relevant_paths_file_path:str):
 
     return pandas.DataFrame.from_dict(data, orient="columns")
 
+def get_video_stream(video_data: dict) -> dict:
+    vstream = next((stream for stream in video_data['streams'] if stream['codec_type'] == 'video'), None)
+    if vstream is None:
+        raise Exception("metadata is not valid or is not from a video source")
+
+    return vstream
+
+def get_total_frames(vstream: dict) -> int:
+    nb_frames = vstream.get('nb_frames')
+    if nb_frames is None:
+        raise Exception("given stream is missing frame data or is not a video stream")
+
+    return int(nb_frames)
+
+def get_fps(vstream: dict) -> float:
+    avg_frame_rate = vstream.get('avg_frame_rate')
+    if avg_frame_rate is None:
+        raise Exception("given stream is missing fps data or is not a video stream")
+    
+    parts = avg_frame_rate.split('/')
+
+    return float(parts[0]) / float(parts[1])
+
+
+def get_range_parts(entry_frames:str) -> list[str]:
+    return entry_frames.split('-')
+
+def get_frames_with_handles(range_parts: list[str], handle_len: float, fps: float) -> Tuple[float, float]:
+    handle_frames = handle_len * fps
+    start = max(float(range_parts[0]) - handle_frames, 0)
+    end = float(range_parts[1]) + handle_frames
+
+    return (start, end)
+
+# needs fps info
+def get_range_handles_below(frame_data: list[dict[str, str]], threshold: int, fps: float) -> list[dict[str, Tuple[float, float]]]:
+    ranges = []
+    for entry in frame_data:
+        entry_frames = entry['Frames']
+        range_parts = get_range_parts(entry_frames)
+        if len(range_parts) == 1:
+            if int(range_parts[0]) >= threshold:
+                return ranges
+
+            continue
+
+        if (int(range_parts[0]) >= threshold) or (int(range_parts[0]) >= threshold):
+            return ranges
+
+        new_frames = get_frames_with_handles(range_parts, HANDLE_LEN, fps)
+        ranges.append({
+            'Path': entry['Path'],
+            'Frames': new_frames
+            })
+
+    return ranges
+
+# unsure if this timecode would work with ffmpeg
+# pretty sure he wants this for output tho
+def frames_to_timecode(frames: int):
+    hours = frames // HOUR_FRAMES
+    frames = frames % HOUR_FRAMES
+
+    minutes = frames // (24 * 60)
+    frames = frames % MINUTE_FRAMES
+    
+    seconds = frames // SECOND_FRAMES
+    frames = frames % SECOND_FRAMES
+
+    return(f'{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}:{str(frames).zfill(2)}')
+
+            
+
+
+def init_mongodb() -> Database:
+    myclient = pymongo.MongoClient("mongodb://localhost:27017")
+    db = myclient['local']
+
+    return db
+
 def main():
-    # baselight then xytech
-    df = get_frame_data("Baselight_export_spring2026.txt", "Xytech_spring2026.txt")
-    print(df)
+    args = conf_argparse()
+
+    baselight = args.baselight
+    xytech = args.xytech
+
+    df = get_frame_data(baselight, xytech)
+    db = init_mongodb()
+    frame_data_col = insert_frame_data(df, db, "frame-data")
+    frame_data = list(frame_data_col.find({}, { '_id': 0 }))
+
+    video_data = ffmpeg.probe(args.process)
+    vstream = get_video_stream(video_data)
+    nb_frames = get_total_frames(vstream)
+    fps = get_fps(vstream)
+
+    ranges = get_range_handles_below(frame_data, nb_frames, fps)
+
+
 
 if __name__ == "__main__":
     main()
